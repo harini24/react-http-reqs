cbc9f381f3c03fc2f00e66ffca24d10ec90233ac
harini <harinisiva1997@gmail.com>
2021-05-24 15:15:26 +0530

    handling http errors

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLocading] = useState(false)
+  const [error, setError] = useState(null)
 
   async function fetchBovieshandler() {
     setIsLocading(true)
-    const response =await fetch('https://swapi.dev/api/films/')
-    const data =await response.json()
-
-    const transformedMovies = data.results.map(movie => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        release: movie.release_date
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      
+      if(!response.ok){
+        throw new Error('Something went wrong')
       }
-    })
-    setMovies(transformedMovies)
-    setIsLocading(false)
+      const data = await response.json()
+      const transformedMovies = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          release: movie.release_date
+        }
+      })
+      setMovies(transformedMovies)
+      setIsLocading(false)
+    } catch (e) {
+      setError(e.message)
+      setIsLocading(false)
+    }
   }
 
   return (
@@ -30,7 +40,8 @@ function App() {
         <button onClick={fetchBovieshandler}>Fetch Movies</button>
       </section>
       <section>
-       {isLoading ? <p>Loading</p> : movies.length > 0 ? <MoviesList movies={movies}/> : <p>found no movies</p> }
+        {isLoading && !error ? <p>Loading</p> : movies.length > 0  ? <MoviesList movies={movies} /> :  !error && <p>found no movies</p>}
+        {!isLoading && error &&<p>{error}</p>}
       </section>
     </React.Fragment>
   );
