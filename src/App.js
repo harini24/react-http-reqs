74406e603cbfb18cbb5eea4f113f309725db7ec3
harini <harinisiva1997@gmail.com>
2021-05-24 14:49:14 +0530

    handling loading and data state

--- src/App.js
+++ src/App.js
@@ -4,36 +4,24 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19', 
-    },
-  ];
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLocading] = useState(false)
 
-  const [movies,setMovies] = useState([])
-  function fetchBovieshandler(){
-    fetch('https://swapi.dev/api/films/')
-    .then(response=>{
-      return response.json()
+  async function fetchBovieshandler() {
+    setIsLocading(true)
+    const response =await fetch('https://swapi.dev/api/films/')
+    const data =await response.json()
+
+    const transformedMovies = data.results.map(movie => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        release: movie.release_date
+      }
     })
-    .then(data=>{
-      const transformedMovies = data.results.map(movie=>{
-        return {id:movie.episode_id,
-          title:movie.title,
-          openingText:movie.opening_crawl,
-          release:movie.release_date}
-      })
-      setMovies(transformedMovies)
-    })
+    setMovies(transformedMovies)
+    setIsLocading(false)
   }
 
   return (
@@ -42,7 +30,7 @@ function App() {
         <button onClick={fetchBovieshandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {isLoading ? <p>Loading</p> : movies.length > 0 ? <MoviesList movies={movies}/> : <p>found no movies</p> }
       </section>
     </React.Fragment>
   );
